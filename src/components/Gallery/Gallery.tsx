5e47e6b99443c701d8081b3eeb25ebe16d475973
Stephen Vaughan <stephen.a.vaughan@gmail.com>
2023-09-18 11:15:33 -0500

    refactor: :art: Consolidated hook and type declaration into Gallery component for demo simplicity

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
-import ImageGallery from "react-image-gallery";
-import useFetchData from '../../hooks/useFetchData'
+import React, { useCallback, useEffect, useState } from 'react';
+import type { ReactImageGalleryItem } from 'react-image-gallery';
+import ImageGallery from 'react-image-gallery';
+
+interface Image {
+    id: string;
+    src: string;
+    srcset: string;
+    alt?: string;
+}
+
+const API_URI = 'https://lineups-api.com/pro/api/apartments/5cdd743acbc13b00043ef391/tour-path/Default'
 
 const Gallery = () => {
 
     const [loaded, setLoaded] = useState(false)
 
-    const images = useFetchData(
-        // Set your own API in the .env file as `VITE_API_URI` then uncomment the line below
-        'https://lineups-api.com/pro/api/apartments/5cdd743acbc13b00043ef391/tour-path/Default'
-    )
+    const [images, setImages] = useState([])
+
+    const dataFetch = useCallback(async () => {
+
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+
+        const requestOptions = { method: 'GET', headers, redirect: 'follow' } as RequestInit;
+        const res = await fetch(API_URI, requestOptions)
+        const data = await res.json()
+        const images = await data.images.map((image: Image, idx: number) => ({
+            original: image.src,
+            srcSet: image.srcset,
+            thumbnail: `${image.src}-/resize/250x/`,
+            originalAlt: image.alt || `demo-image-${idx + 1}`,
+            loading: "eager",
+            thumbnailLoading: "eager",
+        } as ReactImageGalleryItem));
+        setImages(images);
+    }, [])
+
+    useEffect(() => {
+        dataFetch()
+    }, []);
 
     return (
         <div>
